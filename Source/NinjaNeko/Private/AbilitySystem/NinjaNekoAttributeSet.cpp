5a0f3f3e6205dddd5f5d6055834bbb93238aaaba
nektpol <nekocreativetechnologies@gmail.com>
2025-05-25 17:38:33 +0300

    Creating Health, Chakra and Stamina attributes

--- Source/NinjaNeko/Private/AbilitySystem/NinjaNekoAttributeSet.cpp
+++ Source/NinjaNeko/Private/AbilitySystem/NinjaNekoAttributeSet.cpp
@@ -2,4 +2,51 @@
 
 
 #include "AbilitySystem/NinjaNekoAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UNinjaNekoAttributeSet::UNinjaNekoAttributeSet()
+{
+}
+
+void UNinjaNekoAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UNinjaNekoAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UNinjaNekoAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UNinjaNekoAttributeSet, Chakra, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UNinjaNekoAttributeSet, MaxChakra, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UNinjaNekoAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UNinjaNekoAttributeSet, MaxStamina, COND_None, REPNOTIFY_Always);
+}
+
+void UNinjaNekoAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UNinjaNekoAttributeSet,Health, OldHealth);
+}
+
+void UNinjaNekoAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UNinjaNekoAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UNinjaNekoAttributeSet::OnRep_Chakra(const FGameplayAttributeData& OldChakra) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UNinjaNekoAttributeSet, Chakra, OldChakra);
+}
+
+void UNinjaNekoAttributeSet::OnRep_MaxChakra(const FGameplayAttributeData& OldMaxChakra) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UNinjaNekoAttributeSet, MaxChakra, OldMaxChakra);
+}
+
+void UNinjaNekoAttributeSet::OnRep_Stamina(const FGameplayAttributeData& OldStamina) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UNinjaNekoAttributeSet, Stamina, OldStamina);
+}
+
+void UNinjaNekoAttributeSet::OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UNinjaNekoAttributeSet, MaxStamina, OldMaxStamina);
+}
--- Source/NinjaNeko/Public/AbilitySystem/NinjaNekoAttributeSet.h
+++ Source/NinjaNeko/Public/AbilitySystem/NinjaNekoAttributeSet.h
@@ -6,12 +6,64 @@
 #include "AttributeSet.h"
 #include "NinjaNekoAttributeSet.generated.h"
 
-/**
- * 
- */
+// #define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+// GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+// GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+// GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+// GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 UCLASS()
 class NINJANEKO_API UNinjaNekoAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UNinjaNekoAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	//---------------------------------------------------------------------------------
+	//Vital Attributes: Health, Chakra, Stamina 
+	//---------------------------------------------------------------------------------
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	// MaxHealth
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	// Chakra
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Chakra, Category = "Vital Attributes")
+	FGameplayAttributeData Chakra;
+
+	UFUNCTION()
+	void OnRep_Chakra(const FGameplayAttributeData& OldChakra) const;
+
+	// MaxChakra
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxChakra, Category = "Vital Attributes")
+	FGameplayAttributeData MaxChakra;
+
+	UFUNCTION()
+	void OnRep_MaxChakra(const FGameplayAttributeData& OldMaxChakra) const;
+
+	// Stamina
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Stamina, Category = "Vital Attributes")
+	FGameplayAttributeData Stamina;
+
+	UFUNCTION()
+	void OnRep_Stamina(const FGameplayAttributeData& OldStamina) const;
+
+	// MaxStamina
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxStamina, Category = "Vital Attributes")
+	FGameplayAttributeData MaxStamina;
+
+	UFUNCTION()
+	void OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina) const;
 	
 };
